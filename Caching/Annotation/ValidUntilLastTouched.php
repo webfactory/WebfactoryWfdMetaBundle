d9f0ea49d763bb82d4035f8ae57a9e1ac6097b53
Unknown <unknown@kiln.example.com>
2013-11-29 20:11:48 -0100

    Deprecation warning

--- Caching/Annotation/ValidUntilLastTouched.php
+++ Caching/Annotation/ValidUntilLastTouched.php
@@ -6,4 +6,14 @@ namespace Webfactory\Bundle\WfdMetaBundle\Caching\Annotation;
  * @Annotation
  * @Deprecated
  */
-class ValidUntilLastTouched extends Send304IfNotModified {}
+class ValidUntilLastTouched extends Send304IfNotModified {
+    public function __construct($values)
+    {
+        trigger_error(
+            'The ValidUntilLastTouched annotation is deprecated. Use Send304IfNotModified instead.',
+            E_USER_DEPRECATED
+        );
+
+        parent::__construct($values);
+    }
+}
