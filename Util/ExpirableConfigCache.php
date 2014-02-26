1301265d9e1a1825af85daeea21d1431318ee1bf
Matthias Pigulla <mp@webfactory.de>
2014-02-26 09:22:01 -0100

    So wird klarer, was da vorher passierte.

--- Util/ExpirableConfigCache.php
+++ Util/ExpirableConfigCache.php
@@ -3,11 +3,12 @@
 namespace Webfactory\Bundle\WfdMetaBundle\Util;
 
 use Symfony\Component\Config\ConfigCache;
+use Symfony\Component\Finder\Finder;
 
 /**
  * Verhaelt sich wie ConfigCache, ist aber zusätzlich und auch unter !debug nicht mehr fresh,
  * wenn der im Konstruktor gegebene Timestamp sich gegenüber dem Zeitpunkt der Erstellung
- * erhöht hat (für die gleiche Cache-Datei).
+ * geändert hat (für die gleiche Cache-Datei).
  *
  * Diese Implementierung verwendet wechselnde Dateinamen für neuere Generationen,
  * weil so Cache-Datei (die letztlich ja von Klienten includiert wird) von APC gehalten
@@ -17,46 +18,42 @@ use Symfony\Component\Config\ConfigCache;
  * schon aus um zu erkennen, dass es die richtige Generation ist.
  */
 class ExpirableConfigCache extends ConfigCache {
-    protected $timestamp, $metaFile, $baseFile;
+    protected $timestamp, $baseFilename, $timestampedFile;
 
-    public function __construct($file, $debug, $timestamp) {
-        parent::__construct($this->getTimestampedFilePath($file, $timestamp), $debug);
-        $this->baseFile = $file;
-        $this->metaFile = "$file.expire";
+    public function __construct($baseFilename, $debug, $timestamp) {
+        $this->baseFilename = $baseFilename;
+        $this->timestampedFile = "{$baseFilename}_{$timestamp}";
         $this->timestamp = $timestamp;
+
+        parent::__construct($this->timestampedFile, $debug);
     }
 
     public function write($content, array $metadata = null) {
-        $file = $this->metaFile;
-        $oldTs = @file_get_contents($file);
+        parent::write($content, $metadata);
 
-        if ($oldTs <= $this->timestamp) { // <=, weil es sein kann, dass wir den Cache aufgrund anderer Faktoren als dem Timestamp neu machen (think development + geänderte Resourcen)
-            parent::write($content, $metadata);
-            copy(
-                $this->getTimestampedFilePath($this->baseFile, $this->timestamp),
-                $this->baseFile
-            );
-        }
+        /*
+         * Für das Symfony2-Plugin in PHPStorm einen festen Dateinamen vorhalten,
+         * diese Kopie ist ansonsten ohne Relevanz.
+         */ 
+        copy(
+            $this->timestampedFile,
+            $this->baseFilename
+        );
 
-        if ($oldTs < $this->timestamp) { // Wenn wir keine echt neue Generation geschrieben haben, brauchen|dürfen wir das nicht neu schreiben|löschen.
-            $tmpFile = tempnam(dirname($file), basename($file));
-            if (false !== @file_put_contents($tmpFile, $this->timestamp) && @rename($tmpFile, $file)) {
-                chmod($file, 0666);
-                @unlink("{$this->baseFile}_$oldTs");
-                @unlink("{$this->baseFile}_$oldTs.meta");
-            }
-        }
+        $this->cleanup();
     }
 
-    /**
-     * Get the full file system path including the file name for a timestamped version of the $baseFilePath
-     *
-     * @param string $baseFilePath full file system path to the base file
-     * @param int $timestamp
-     * @return string
-     */
-    protected function getTimestampedFilePath($baseFilePath, $timestamp)
-    {
-        return $baseFilePath . '_' . $timestamp;
+    protected function cleanup() {
+        $finder = new Finder();
+        $basename = basename($this->baseFilename);
+        $files = $finder->files()
+                        ->in(dirname($this->baseFilename))
+                        ->depth('== 0')
+                        ->name("#{$basename}_#")
+                        ->notName("#{$basename}_{$this->timestamp}#");
+
+        foreach ($files as $file) {
+            @unlink($file->getRealpath());
+        }
     }
 }
