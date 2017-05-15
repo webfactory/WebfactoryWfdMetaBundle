973d04ee49389d17fe31d62c732e1f21fa4843cd
Malte Wunsch <mw@webfactory.de>
2017-05-15 13:15:23 +0200

    Removed merge mess

--- Provider.php
+++ Provider.php
@@ -128,48 +128,4 @@ class Provider
         $lastTouchedObject = new \DateTime($lastTouched);
         return $lastTouchedObject->getTimestamp();
     }
-
-    protected function cache(array $namesOrIds)
-    {
-        $ids = array();
-        $names = array();
-
-        foreach ($namesOrIds as $t) {
-            $this->cache[$t] = 0; // prevent re-query
-            if ($t == '*') {
-                $lastTouchOnAnyTable = $this->connection->fetchAssoc('SELECT MAX(last_touched) lastTouchedString FROM wfd_meta');
-
-                if ($lastTouchOnAnyTable['lastTouchedString'] === false) {
-                    $this->cache['*'] = null;
-                } else {
-                    $lastTouchedObject = new \DateTime($lastTouchOnAnyTable['lastTouchedString']);
-                    $this->cache['*'] = $lastTouchedObject->getTimestamp();
-                }
-            } elseif (is_numeric($t)) {
-                $ids[] = $t;
-            } else {
-                $names[] = $t;
-            }
-        }
-
-        if ($names || $ids) {
-            $result = $this->connection->fetchAll('
-                    SELECT t.id, t.tablename, MAX(m.last_touched) lastTouchedString
-                    FROM wfd_meta m
-                    JOIN wfd_table t on m.wfd_table_id = t.id
-                    WHERE '
-                    .($ids ? ('t.id IN ('.implode(', ', array_fill(0, count($ids), '?')).')') : '')
-                    .(($ids && $names) ? ' OR ' : '')
-                    .($names ? ('t.tablename IN ('.implode(', ', array_fill(0, count($names), '?')).')') : '').'
-                    GROUP BY t.id
-                ',
-                array_merge($ids, $names)
-            );
-
-            foreach ($result as $row) {
-                $lastTouchedObject = new \DateTime($row['lastTouchedString']);
-                $this->cache[$row['id']] = $this->cache[$row['tablename']] = $lastTouchedObject->getTimestamp();
-            }
-        }
-    }
 }
