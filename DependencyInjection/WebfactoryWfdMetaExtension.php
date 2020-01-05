0d130643ba0f355d5acbc2597b3656debb2e59e2
Matthias Pigulla <mp@webfactory.de>
2020-01-05 21:37:01 +0000

    Rename services to support autowiring

--- DependencyInjection/WebfactoryWfdMetaExtension.php
+++ DependencyInjection/WebfactoryWfdMetaExtension.php
@@ -11,13 +11,19 @@ namespace Webfactory\Bundle\WfdMetaBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class WebfactoryWfdMetaExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $fileLocator = new FileLocator(__DIR__ . '/../Resources/config');
+
+        $xmlLoader = new XmlFileLoader($container, $fileLocator);
+        $xmlLoader->load('services.xml');
+
+        $yamlLoader = new YamlFileLoader($container, $fileLocator);
+        $yamlLoader->load('legacy_aliases.yml');
     }
 }
