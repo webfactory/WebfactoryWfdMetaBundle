dc9266e66446795ea832734edfb0b47e1be58914
Matthias Pigulla <mp@webfactory.de>
2025-11-24 15:53:15 +0100

    Stelle von XML- auf PHP-basierte Service Konfiguration um (#56)
    XML-basierte Service-Konfiguration ist deprecated seit
    https://github.com/symfony/symfony/pull/60568 und wird in Symfony 8.0
    nicht mehr unterstützt.
    
    Vgl. auch
    https://symfony.com/blog/new-in-symfony-7-4-deprecated-xml-configuration.

---
+++ src/Resources/config/orm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $container) {
+    $services = $container->services();
+    $parameters = $container->parameters();
+
+    $services->defaults()
+        ->private()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->set(\Webfactory\Bundle\WfdMetaBundle\DoctrineMetadataHelper::class)
+        ->args([expr('
+                service("doctrine.orm.entity_manager").getMetadataFactory()
+            ')]);
+};
