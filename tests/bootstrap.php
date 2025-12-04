fa2ccd3c99d46558396da3a2ea3d61d12042b145
mpdude <1202333+mpdude@users.noreply.github.com>
2025-12-04 13:16:30 +0000

    Fix CS with PHP-CS-Fixer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,9 @@
 
 use Symfony\Component\ErrorHandler\DebugClassLoader;
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+require_once __DIR__.'/../vendor/autoload.php';
 
 // ensure a fresh cache every time tests are run
-(new \Symfony\Component\Filesystem\Filesystem())->remove(__DIR__.'/Fixtures/var/cache/test');
+(new Symfony\Component\Filesystem\Filesystem())->remove(__DIR__.'/Fixtures/var/cache/test');
 
 DebugClassLoader::enable();
