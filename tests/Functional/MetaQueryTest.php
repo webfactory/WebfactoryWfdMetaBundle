3ada19792ae9d6f6257dd31bbb8634d54af7f8de
mpdude <1202333+mpdude@users.noreply.github.com>
2025-12-04 12:58:31 +0000

    Fix CS with PHP-CS-Fixer

--- tests/Functional/MetaQueryTest.php
+++ tests/Functional/MetaQueryTest.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Webfactory\Bundle\WfdMetaBundle\Tests\Functional;
 
-use Doctrine\DBAL\Connection;
+use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use PHPUnit\Framework\Attributes\Test;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Webfactory\Bundle\WfdMetaBundle\MetaQueryFactory;
-use Webfactory\Bundle\WfdMetaBundle\Tests\Fixtures\CreateSchemaHelper;
 use Webfactory\Bundle\WfdMetaBundle\Tests\Fixtures\Entity\TestEntity;
 
 class MetaQueryTest extends FunctionalTestCase
@@ -26,7 +24,7 @@ class MetaQueryTest extends FunctionalTestCase
     #[Test]
     public function getLastTouched_for_entity_class(): void
     {
-        $time = new \DateTimeImmutable();
+        $time = new DateTimeImmutable();
 
         $this->dbal->insert('wfd_table', ['id' => 42, 'tablename' => 'test_table']);
         $this->dbal->insert('wfd_meta', ['data_id' => 1, 'wfd_table_id' => 42, 'last_touched' => $time], ['last_touched' => Types::DATETIME_IMMUTABLE]);
